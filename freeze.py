0d87f5b63299c1721d64ecc2c078f42baac78ec8
Charles Mesa Cayobit <charlesmesacayobit@gmail.com>
2025-12-20 13:46:31 -0800

    Update frozen files

--- freeze.py
+++ freeze.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+from pathlib import Path
+
 from flask_frozen import Freezer
 
 from app import app
@@ -7,3 +9,9 @@ from app import app
 if __name__ == "__main__":
     freezer = Freezer(app)
     freezer.freeze()
+
+    for file in Path(__file__).with_name("app").joinpath("build").iterdir():
+        if file.is_file() and not file.suffix:
+            new_name = file.with_suffix(".html")
+
+            file.rename(new_name)
